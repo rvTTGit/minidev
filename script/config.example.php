8d24aa22e6b33f2213d0ab84aa4f2a12edb21fef
rvTTGit <mail@herve-thomas.fr>
2018-01-25 20:10:15 +0100

    ménage

--- script/config.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-// On declare un tableau de configuration vide
-$config = [];
-$config['dbname'] = '***';
-$config['dbhost'] = '***';
-$config['dbuser'] = '***';
-$config['dbpassword'] = '***';
-
- 
